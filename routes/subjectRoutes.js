d9a60264f620ec30a186b74a4301fb99b2603042
DANIEL WILLIAMS MENSAH <83681534+Williams9007@users.noreply.github.com>
2025-11-28 10:49:05 -0600

    Update subjectRoutes.js

--- routes/subjectRoutes.js
+++ routes/subjectRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getSubjectsByPackage } from "../controllers/subjectcontroller.js";
+import { getSubjectsByPackage } from "../Controllers/subjectController.js";
 
 const router = express.Router();
 
