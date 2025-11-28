c263fb78133b9263a978324e407e2d1e91e77cf4
DANIEL WILLIAMS MENSAH <83681534+Williams9007@users.noreply.github.com>
2025-11-28 09:31:12 -0600

    Update subjectRoutes.js

--- routes/subjectRoutes.js
+++ routes/subjectRoutes.js
@@ -3,6 +3,7 @@ import { getSubjectsByPackage } from "../controllers/subjectController.js";
 
 const router = express.Router();
 
+// Route to fetch subjects by package and optional grade
 router.get("/by-package/:package", getSubjectsByPackage);
 
 export default router;
