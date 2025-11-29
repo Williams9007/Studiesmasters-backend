490c4a2b6646b46be4e282dcf0357c47a56e5dd6
DANIEL WILLIAMS MENSAH <83681534+Williams9007@users.noreply.github.com>
2025-11-29 09:55:35 -0600

    Update QaoUser.js

--- models/QaoUser.js
+++ models/QaoUser.js
@@ -1,11 +1,10 @@
-// backend/models/QaoUser.js
 import mongoose from "mongoose";
 
 const QaoUserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  role: { type: String, required: true, default: "qao" }, // added role
   assignedSubjects: [{ type: mongoose.Schema.Types.ObjectId, ref: "Subject" }],
-  // You can add more fields like role, password, etc.
 }, { timestamps: true });
 
 const QaoUser = mongoose.model("QaoUser", QaoUserSchema);
