0231fff27731cd607d6dd993311da9f39af26b60
DANIEL WILLIAMS MENSAH <83681534+Williams9007@users.noreply.github.com>
2026-07-16 03:20:40 -0700

    Fix student registration subject handling

---
+++ data/curriculumCatalog.js
@@ -0,0 +1,10 @@
+export const curriculumCatalog = {
+  GES: {
+    grades: ["Primary 1", "Primary 2", "Primary 3", "Primary 4", "Primary 5", "Primary 6", "JHS 1", "JHS 2", "JHS 3", "SHS 1", "SHS 2", "SHS 3"],
+    subjects: ["English", "Maths", "Science"],
+  },
+  CAMBRIDGE: {
+    grades: ["Grade 1", "Grade 2", "Grade 3", "Grade 4", "Grade 5", "Grade 6", "Grade 7", "Grade 8", "Grade 9"],
+    subjects: ["English", "Maths", "Science"],
+  },
+};
